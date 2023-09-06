e97b8e26074082ef7354d41dc10cd1e5ffe63af6
George Zalepa <george.zalepa@gmail.com>
2023-09-06 18:17:08 -0400

    Refactor

--- app.js
+++ app.js
@@ -8,13 +8,17 @@ app.set('view engine', 'pug');
 
 app.use(express.static('public'))
 
+const DATA = [
+  { date: 'April 12, 2023', days: getDate(2023, 4, 12),  text: 'Jersey City promised safety improvements on St. Pauls Avenue.'                            },
+  { date: 'Jun. 11, 2021',  days: getDate(2021, 6, 11),  text: 'a vehicle struck a pedestrian on St. Pauls Avenue triggering more calls for safety.'      },
+  { date: 'Sept. 17, 2020', days: getDate(2020, 9, 17),  text: 'Jersey City conducted a walking audit to experience the problems on St. Pauls Avenue'     },
+  { date: 'Nov. 19, 2011',  days: getDate(2011, 11, 19), text: 'a grandmother was killed on St. Pauls Avenue while picking up her grandchild from PS 31.' },
+  // { date: 'Jul. 12, 2011',  days: getDate(2011, 7, 12),  text: 'a pedestrian was impaled on the fence at PS 31 after being struck by a vehicle.'          }
+];
+
 app.get('/', (_, res) => {
-  res.render('home', { 
-    council: { date: 'April 12, 2023', days: getDate(2023, 4, 12) },
-    pedestrianStruck: {date: 'Jun. 11, 2021', days: getDate(2021, 6, 11) },
-    walkingAudit: {date: 'Sept. 17, 2020', days: getDate(2020, 9, 17) },
-    grandma: {date: 'Nov. 19, 2011', days: getDate(2011, 11, 19) },
-  });
+  const events = DATA.sort((a, b) => a.days - b.days);
+  res.render('home', { events });
 });
 
 app.get('/api', (_, res) => {

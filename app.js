65b0e1d01659af7792375c292674c58a858316c2
George Zalepa <george.zalepa@gmail.com>
2023-09-05 22:42:34 -0400

    Style app

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ app.set('view engine', 'pug');
 app.use(express.static('public'))
 
 app.get('/', (_, res) => {
-  res.render('home', { diff: getDate() });
+  res.render('home', { 
+    council: { date: 'April 12, 2023', days: getDate(2023, 4, 12) },
+    pedestrianStruck: {date: 'Jun. 11, 2021', days: getDate(2021, 6, 11) },
+    walkingAudit: {date: 'Sept. 17, 2020', days: getDate(2020, 9, 17) },
+  });
 });
 
 app.get('/api', (_, res) => {
--- util.js
+++ util.js
@@ -1,5 +1,6 @@
-function getDate() {
-  const pastDate = new Date(2023, 3, 12);
+function getDate(year, month, day) {
+  // const pastDate = new Date(2023, 3, 12);
+  const pastDate = new Date(year, month - 1, day);
   const currentDate = new Date();
   const differenceInMs = currentDate.getTime() - pastDate.getTime();
   const differenceInDays = differenceInMs / 86400000;
